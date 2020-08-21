aebdad10bdfc5e2dabe3df3c971ecfa530fce42d
Matheus Afonso <matheusafonsouza@gmail.com>
2020-08-20 21:25:46 -0300

    feature: Created header menu

---
+++ src/components/Menu/index.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import QRCode from 'react-native-qrcode-svg';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import {
+  Container,
+  Code,
+  Nav,
+  NavItem,
+  NavText,
+  SignOutButton,
+  SignOutButtonText,
+} from './styles';
+
+export default function Menu() {
+  return (
+    <Container>
+      <Code>
+        <QRCode
+          value="https://www.google.com.br"
+          size={80}
+          color="#fff"
+          backgroundColor="#8b10ae"
+        />
+      </Code>
+
+      <Nav>
+        <NavItem>
+          <Icon name="help-outline" size={20} color="#fff" />
+          <NavText>Me ajuda</NavText>
+        </NavItem>
+        <NavItem>
+          <Icon name="person-outline" size={20} color="#fff" />
+          <NavText>Perfil</NavText>
+        </NavItem>
+        <NavItem>
+          <Icon name="credit-card" size={20} color="#fff" />
+          <NavText>Configurar cartão</NavText>
+        </NavItem>
+        <NavItem>
+          <Icon name="smartphone" size={20} color="#fff" />
+          <NavText>Configurações do app</NavText>
+        </NavItem>
+      </Nav>
+
+      <SignOutButton onPress={() => {}}>
+        <SignOutButtonText>SAIR DO APP</SignOutButtonText>
+      </SignOutButton>
+    </Container>
+  );
+}
